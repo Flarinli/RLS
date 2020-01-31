2cd98d9066a17cb412a8d6156e0153e22d300921
Flarinli <flarinli@rambler.ru>
2020-01-31 09:56:20 +0300

    Добавлен интегратор

---
+++ IIntegrator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLC
+{
+    interface IIntegrator
+    {
+        double[] Step_Of_Integrator(double[] In, RightValue[] Rvalues);
+
+    }
+}
---
+++ Integrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RLC
+{
+    public class Euler_Integrator : IIntegrator
+    {
+        public double[] Step_Of_Integrator(double[] X, RightValue[] rF)
+
+        {
+            X[0] += X[2] * rF[0](X) * X[6];
+            X[1] += X[2] * rF[1](X) * X[6];
+            X[2] += rF[2](X) * X[6];
+            return X;
+        }
+        public Euler_Integrator()
+        {
+
+        }
+    }
+}
